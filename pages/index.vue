669e7b92d17baa47e128325a7bb82ca735bda306
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-24 09:50:04 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const name: string = "ciao";
+</script>
+
+<template>
+  <div class="bg-blue-500 text-4xl">
+    <h1 class="italic font-black">{{ name }}</h1>
+  </div>
+</template>
