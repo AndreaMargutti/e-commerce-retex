0724ad4f6565f474effa384c0c521cfbe9eec903
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-25 16:23:35 +0100

    add scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
-  css: ["~/assets/css/main.css"],
+  css: ["~/assets/scss/main.scss"],
   vite: {
     plugins: [tailwindcss()],
   },
