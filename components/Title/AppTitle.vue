557acfd924989f6598dbe5777a1e0a1afe6bacb5
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-26 12:04:48 +0100

    remove apptitle

--- components/Title/AppTitle.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
-  </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,1 @@
-<template>
-  <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
-</template>
+<template></template>
