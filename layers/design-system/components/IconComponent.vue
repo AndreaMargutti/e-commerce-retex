61bdb2ac3c591b7908abfe6cf32b1bbd5d50d853
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-27 09:43:19 +0100

    finish import of icons

--- layers/design-system/components/IconComponent.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <!-- TODO: finire di importare le icone e cambiare i nomi -->
-  <div class="bg-blue-300 h-23 w-50 text-red-500">
-    <Icon name="my-icon:wishlist" size="3em" />
-  </div>
-</template>
