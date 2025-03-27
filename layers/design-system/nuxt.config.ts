0929fc3381b88de6027d53f5238f0c7d6d6c58f6
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-27 12:14:37 +0100

    [refactor]: move asstes in design-system layer

--- layers/design-system/nuxt.config.ts
+++ layers/design-system/nuxt.config.ts
@@ -1,12 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/fonts"],
-  css: ["~/assets/css/main.css"],
+  css: ["./layers/design-system/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/fonts"],
-  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
