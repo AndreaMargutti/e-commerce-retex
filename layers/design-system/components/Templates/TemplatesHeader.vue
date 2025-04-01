fba707a95af380a5e2607b3bcb7f75d26720d3bd
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-01 12:31:00 +0200

    refactor: TemplateHeader.vue: use semantic html tag and change display of AtomsLink div

--- layers/design-system/components/Templates/TemplatesHeader.vue
+++ layers/design-system/components/Templates/TemplatesHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <section
+  <header
     class="flex max-lg:justify-between lg:text-end items-center py-2 px-4 md:px-8"
   >
     <div class="space-x-6 lg:space-x-4 px-4 lg:order-2 lg:grow">
@@ -11,7 +11,7 @@
       <AtomsLogoDefault />
     </div>
 
-    <div class="flex grow gap-4">
+    <div class="hidden lg:flex grow gap-4">
       <AtomsLink v-for="n in 10" name="HOME" href="/" />
     </div>
 
@@ -19,5 +19,5 @@
       <AtomsIcon name="search" />
       <AtomsIcon name="cart" />
     </div>
-  </section>
+  </header>
 </template>
