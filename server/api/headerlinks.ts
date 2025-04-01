f636a05303949e28e2a07cefa4ee6958bef32bfb
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-01 17:27:23 +0200

    chore: add json for header links

--- server/api/headerlinks.ts
+++ server/api/headerlinks.ts
@@ -2,34 +2,42 @@ export default defineEventHandler((event) => {
   return {
     headerLinks: [
       {
+        id: 1,
         label: "Home",
         href: "/",
       },
       {
+        id: 2,
         label: "Sale",
         href: "/sale",
       },
       {
+        id: 3,
         label: "New Arrivals",
         href: "/new-arrivals",
       },
       {
+        id: 4,
         label: "Women",
         href: "/women",
       },
       {
+        id: 5,
         label: "Men",
         href: "/men",
       },
       {
+        id: 6,
         label: "Kids",
         href: "/kids",
       },
       {
+        id: 7,
         label: "Gift Guide",
         href: "/gift-guide",
       },
       {
+        id: 8,
         label: "Woolrich World",
         href: "/woolrich-world",
       },
