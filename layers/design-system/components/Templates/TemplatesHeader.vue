6f0a3f3847b4342ee8689136dadd113e36aafb1b
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-01 17:27:50 +0200

    fix: correct header internal design

--- layers/design-system/components/Templates/TemplatesHeader.vue
+++ layers/design-system/components/Templates/TemplatesHeader.vue
@@ -1,23 +1,50 @@
+<script setup lang="ts">
+const { headerLinks, init } = useHeaderLinks();
+init();
+</script>
+
 <template>
   <header
     class="flex max-lg:justify-between lg:text-end items-center py-2 px-4 md:px-8"
   >
     <div class="space-x-6 lg:space-x-4 px-4 lg:order-2 lg:grow">
-      <AtomsIcon name="menu" />
+      <span class="lg:hidden">
+        <AtomsIcon name="menu" class="hidden" />
+      </span>
       <AtomsIcon name="account" />
     </div>
 
-    <div class="mx-[37px]">
+    <div class="lg:pr-4 min-[1440px]:pr-10">
       <AtomsLogoDefault />
     </div>
 
-    <div class="hidden lg:flex grow gap-4">
-      <AtomsLink v-for="n in 10" name="HOME" href="/" />
+    <div class="hidden lg:flex grow gap-4 min-[1440px]:gap-10">
+      <AtomsLink
+        v-for="{ id, label, href } in headerLinks"
+        :name="label.toLocaleUpperCase()"
+        :href="href"
+        :key="id"
+      />
     </div>
 
-    <div class="space-x-6 lg:space-x-4 lg:order-3">
-      <AtomsIcon name="search" />
+    <div class="space-x-6 lg:space-x-4 lg:order-3 lg:relative">
+      <span class="search-icon">
+        <AtomsIcon name="search" />
+      </span>
+      <span class="max-lg:hidden space-x-4">
+        <AtomsIcon name="pin" />
+        <AtomsIcon name="wishlist" />
+      </span>
       <AtomsIcon name="cart" />
     </div>
   </header>
 </template>
+
+<style scoped>
+@media screen and (min-width: 1024px) {
+  .search-icon {
+    position: absolute;
+    left: calc(-100% + 16px);
+  }
+}
+</style>
