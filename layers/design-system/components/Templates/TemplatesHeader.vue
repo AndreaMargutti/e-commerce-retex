8df5f5f7aef8caca8ba1cda064892daa85be6575
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-04 15:04:39 +0200

    fix: [TemplateHeader] add background-color

--- layers/design-system/components/Templates/TemplatesHeader.vue
+++ layers/design-system/components/Templates/TemplatesHeader.vue
@@ -12,7 +12,7 @@ const isLogged = ref(false);
 
 <template>
   <header
-    class="flex max-lg:justify-between lg:text-end items-center py-2 px-4 md:px-8"
+    class="bg-white flex max-lg:justify-between lg:text-end items-center py-2 px-4 md:px-8"
   >
     <div class="space-x-6 lg:space-x-4 px-4 lg:order-2 lg:grow">
       <span class="lg:hidden">
