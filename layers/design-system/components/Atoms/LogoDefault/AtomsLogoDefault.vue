5f0ea3bf4cdadddee6c4f5b4aa8abd62c86f04e5
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-07 17:12:48 +0200

    fix: [AtomsLogoDefault.vue] add link

--- layers/design-system/components/Atoms/LogoDefault/AtomsLogoDefault.vue
+++ layers/design-system/components/Atoms/LogoDefault/AtomsLogoDefault.vue
@@ -1,6 +1,8 @@
 <template>
-  <Icon
-    name="icon:logo-default"
-    class="text-[4rem] lg:text-[51px] xl:text-[4.5rem]"
-  />
+  <NuxtLink to="/">
+    <Icon
+      name="icon:logo-default"
+      class="text-[4rem] lg:text-[51px] xl:text-[4.5rem]"
+    />
+  </NuxtLink>
 </template>
