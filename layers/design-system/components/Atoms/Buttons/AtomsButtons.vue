c2f40d527290fe6fa3601a0cca8e77c518034816
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-07 10:55:59 +0200

    feat: [AtomsButton] add props

--- layers/design-system/components/Atoms/Buttons/AtomsButtons.vue
+++ layers/design-system/components/Atoms/Buttons/AtomsButtons.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup>
 import type { AtomsButtonsProps } from "./AtomsButtonsProps.ts";
-defineProps<AtomsButtonsProps>();
+const props = defineProps<AtomsButtonsProps>();
+
+const typeClass = computed(() => {
+  switch (props.type) {
+    case "tertiary":
+      return "text-sm";
+  }
+});
 </script>
 <template>
-  {{ type }}
-  <button class="text-sm">{{ label }}</button>
+  <!-- {{ typeClass }} -->
+  <button :class="typeClass" class="flex gap-2 items-center">
+    <p v-if="isUppercase">
+      {{ label.toLocaleUpperCase() }}
+    </p>
+    <p v-else>{{ label }}</p>
+    <AtomsIcon name="navigation-right" v-if="hasIcon" />
+  </button>
 </template>
