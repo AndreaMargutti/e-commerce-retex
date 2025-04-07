3c7b02d91268c35966a860097b1a70d9e623fc31
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-07 17:02:29 +0200

    fix: [TemplatesHeader.vue] removed max:lg

--- layers/design-system/components/Templates/TemplatesHeader.vue
+++ layers/design-system/components/Templates/TemplatesHeader.vue
@@ -13,7 +13,7 @@ const isLogged = ref(false);
 
 <template>
   <header
-    class="bg-white flex max-lg:justify-between lg:text-end items-center py-2 px-4 md:px-8"
+    class="bg-white flex justify-between lg:text-end items-center py-2 px-4 md:px-8"
   >
     <div class="space-x-6 lg:space-x-4 px-4 lg:order-2 lg:grow">
       <span class="lg:hidden">
