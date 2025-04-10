0dad838877d30c29f0b41485c27b8a54678b6db1
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-10 17:15:33 +0200

    feat: [MoleculesList] add first draft of MoleculesList

--- layers/design-system/components/Molecules/List/MoleculesList.vue
+++ layers/design-system/components/Molecules/List/MoleculesList.vue
@@ -1,5 +1,20 @@
+<script lang="ts" setup>
+const { data } = useFetch("/api/menu");
+const menu = data.value?.items;
+console.log(menu);
+
+function hasItems(item: MenuItem): boolean {
+  return item.items !== undefined && item.items.length > 0;
+}
+</script>
+
 <template>
   <ul>
-    <li v-for="n in 10">label</li>
+    <li v-for="item in menu">
+      <AtomsButton
+        :label="item.label"
+        :icon="{ hasIcon: hasItems(item), iconName: 'navigation-right' }"
+      />
+    </li>
   </ul>
 </template>
