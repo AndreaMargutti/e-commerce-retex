d14c17c3cd087cee6cf2612717b164282545284d
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-11 09:37:07 +0200

    fix: [MolecluesList] fix logic of toggleMenu

--- layers/design-system/components/Molecules/List/MoleculesList.vue
+++ layers/design-system/components/Molecules/List/MoleculesList.vue
@@ -3,41 +3,43 @@ const { data } = useFetch("/api/menu");
 const menu = data.value?.items;
 console.log(menu);
 
-function menuItems(menu: MenuItem) {
-  if (menu.items !== undefined && menu.items.length > 0) {
-    console.log(menu.items);
-    return menu.items;
-  }
-}
-
 function hasItems(item: MenuItem): boolean {
   return item.items !== undefined && item.items.length > 0;
 }
 
 const isOpen = ref(false);
+const secondLayerItems = ref<MenuItem[] | null>(null);
 
-const openMenu = () => {
+const toggleMenu = () => {
   console.log("open menu");
   isOpen.value = !isOpen.value;
+};
+
+const openSecondLayer = (item: MenuItem) => {
+  if (hasItems(item)) {
+    secondLayerItems.value = item.items;
+    isOpen.value = true;
+  }
 };
 </script>
 
 <template>
   <div class="relative">
     <ul v-if="!isOpen">
-      <li v-for="item in menu">
+      <li v-for="item in menu" :key="item.id">
         <AtomsButton
           :label="item.label"
           :icon="{ hasIcon: hasItems(item), iconName: 'navigation-right' }"
-          @click="openMenu"
+          @click="hasItems(item) ? openSecondLayer(item) : null"
         />
       </li>
     </ul>
-    <!--TODO: finire secondo livello del menu-->
     <div class="absolute top-0 left-0" v-if="isOpen">
-      <div @click="openMenu">Torna Indietro</div>
+      <div @click="toggleMenu">Torna Indietro</div>
       <ul>
-        <li v-for="item in menuItems(menu)"></li>
+        <li v-for="item in secondLayerItems" :key="item.id">
+          <AtomsButton :label="item.label" />
+        </li>
       </ul>
     </div>
   </div>
