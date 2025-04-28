8c128d1b7a802a59c39498d873f58f1d585b0836
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-28 12:24:44 +0200

    fix: [MenuDesktop] adjust layer structure for correct rendering

--- layers/design-system/components/Molecules/DesktopMenu/MoleculesDesktopMenu.vue
+++ layers/design-system/components/Molecules/DesktopMenu/MoleculesDesktopMenu.vue
@@ -4,7 +4,13 @@ defineProps<MoleculesDesktopMenuProps>();
 </script>
 
 <template>
-  <div class="absolute inset-y-full left-0 right-0">
-    <MoleculesList :items="items" />
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <AtomsLink :name="item.label" :href="item.to" variant="header">
+          <MoleculesList :items="item.items || []" />
+        </AtomsLink>
+      </li>
+    </ul>
   </div>
 </template>
--- layers/design-system/components/Molecules/List/MoleculesList.vue
+++ layers/design-system/components/Molecules/List/MoleculesList.vue
@@ -6,10 +6,12 @@ defineProps<MoleculesListProps<MenuItem>>();
 
 <template>
   <ul>
-    <li v-for="(item, index) in items" :key="index" class="">
-      <AtomsLink :name="item.label" :href="item.to" variant="header">
-        <!-- <MoleculesList v-if="item.items" :items="item.items" /> -->
-      </AtomsLink>
+    <li v-for="singleItem in items">
+      <AtomsLink
+        :name="singleItem.label"
+        :href="singleItem.to"
+        variant="header"
+      />
     </li>
   </ul>
 </template>
