2d22270bd64a33c705138db2067e64446133b9b9
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-28 12:28:52 +0200

    chore: add "see all"

--- layers/design-system/components/Molecules/DesktopMenu/MoleculesDesktopMenu.vue
+++ layers/design-system/components/Molecules/DesktopMenu/MoleculesDesktopMenu.vue
@@ -9,6 +9,7 @@ defineProps<MoleculesDesktopMenuProps>();
       <li v-for="(item, index) in items" :key="index">
         <AtomsLink :name="item.label" :href="item.to" variant="header">
           <MoleculesList :items="item.items || []" />
+          <p>VEDI TUTTI</p>
         </AtomsLink>
       </li>
     </ul>
