c8d1690f607c426eb55d06a9d10d81fc8cacdfcd
AndreaMargutti <marguttiandrea30@gmail.com>
2025-04-29 12:37:02 +0200

    feat: add MoleculesMobileMenu story for Storybook

---
+++ layers/design-system/components/Molecules/MobileMenu/MoleculesMobileMenu.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import MoleculesMobileMenu from "./MoleculesMobileMenu.vue";
+
+const meta: Meta<typeof MoleculesMobileMenu> = {
+  title: "Design System/Molecules/Mobile Menu",
+  component: MoleculesMobileMenu,
+};
+export default meta;
+
+type Story = StoryObj<typeof MoleculesMobileMenu>;
+
+export const Default: Story = {
+  args: {
+    statusMenu: true,
+  },
+  render: (args) => ({
+    components: { MoleculesMobileMenu },
+    setup() {
+      return { args };
+    },
+    template: '<MoleculesMobileMenu v-bind="args" />',
+  }),
+};
