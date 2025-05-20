8fa7efb41c283df9d636c0c22194d29b1a779de9
AndreaMargutti <marguttiandrea30@gmail.com>
2025-05-20 11:34:24 +0200

    refactor: update AtomsBadge component to include inverted prop and adjust size handling

--- layers/design-system/components/Atoms/Badge/AtomsBadge.vue
+++ layers/design-system/components/Atoms/Badge/AtomsBadge.vue
@@ -1,38 +1,50 @@
 <script setup lang="ts">
 import type { AtomsBadgeProps } from "./AtomsBadgeProps";
-const props = defineProps<AtomsBadgeProps>();
+const props = withDefaults(defineProps<AtomsBadgeProps>(), {
+  size: "large",
+  inverted: false,
+});
 
 const label = computed((): string | number => {
   return props.items > 99 ? `99+` : props.items;
 });
 
-const textSize = computed((): string => {
-  if (props.items < 99) {
-    return "text-sm h-[20px] w-[20px]";
-  } else if (props.items === 99) {
-    return "text-tiny";
-  } else {
-    return "text-[7px]";
-  }
+const badgeSize = computed((): string => {
+  if (props.size === "small") {
+    return "h-3.5 w-3.5";
+  } else if (props.size === "large") {
+    return "h-5 w-5";
+  } else return "";
 });
 
-const color = computed((): string => {
-  if (props.color === "grey") {
-    return "bg-grey-base text-black-base";
-  } else {
+const textSize = computed((): string => {
+  if (props.size === "small") {
+    if (props.items > 99) {
+      return "text-[7px]";
+    } else {
+      return "text-tiny";
+    }
+  }
+  return "text-sm";
+});
+
+const isInverted = computed(() => {
+  if (props.inverted) {
     return "bg-black-base text-white";
+  } else {
+    return "bg-gray-base text-black-base";
   }
 });
 
 const badgeStyle = computed((): string[] => {
-  return [textSize.value, color.value];
+  return [badgeSize.value, textSize.value, isInverted.value];
 });
 </script>
 
 <template>
   <div
     :class="badgeStyle"
-    class="h-[14px] w-[14px] py-0.5 px-1 rounded-full flex items-center justify-center"
+    class="py-0.5 px-1 rounded-full flex items-center justify-center"
   >
     <p>{{ label }}</p>
   </div>
