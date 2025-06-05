93527b288f95ee3cc5f7ae1b2742c5f897e438c4
Andrea Margutti <marguttiandrea30@gmail.com>
2025-06-05 14:31:24 +0200

    fix: update image URLs for improved content delivery in pageContent API

--- server/api/pageContent.ts
+++ server/api/pageContent.ts
@@ -3,7 +3,7 @@ export default defineEventHandler(() => {
         "en/": [
             {
                 name: "HeroBanner",
-                image: "https://picsum.photos/1200/600",
+                image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/9e066b5b-a554-4e04-af93-68c4f68aa1f7/fdynkn/std/1024x960/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                 imageAlt: "Diversity and Inclusion",
                 title: "Diversity and Inclusion",
             },
@@ -13,7 +13,7 @@ export default defineEventHandler(() => {
             },
             {
                 name: "HeroBanner",
-                image: "https://picsum.photos/1200/600",
+                image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/70a6027d-9c78-4465-9303-cad0f087f13e/fdynkn/std/1024x960/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                 imageAlt: "Emphaty will always remain the strongest fiber",
                 title: "Emphaty will always remain the strongest fiber",
             },
@@ -24,10 +24,10 @@ export default defineEventHandler(() => {
             {
                 name: "FourImage",
                 images: [
-                    {src: "link", alt: "Immagine 1"},
-                    {src: "link", alt: "Immagine 2"},
-                    {src: "link", alt: "Immagine 3"},
-                    {src: "link", alt: "Immagine 4"}
+                    {src: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/ec7bc695-e1d8-48e1-8e16-867e12c98e79/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120", alt: "Immagine 1"},
+                    {src: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/d09239b9-4d94-4010-aa76-f44ea1ea5d2f/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120", alt: "Immagine 2"},
+                    {src: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/bebea67e-de59-4ed8-94ea-6c2f3da45aa4/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120", alt: "Immagine 3"},
+                    {src: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/f9e77df3-306c-4b4b-9d4d-d0ada3b8815c/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120", alt: "Immagine 4"}
                 ],
                 title: "Woolrich embraces and empowers all communities",
             },
@@ -38,7 +38,7 @@ export default defineEventHandler(() => {
                     {
                         title: "Being a woman today",
                         text: "For this international Women's Day, we recognize five women who are making strides in their own right. Carolina Amoretti, Luzai, Sara Ricciardi, Aya Mohamed, and Bianca Hirata let us into their private spaces and discuss various aspects of womanhood that inspire them",
-                        image: "https://picsum.photos/400/300",
+                        image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/ec4c47d3-3ce6-40c5-aa23-d9e1c9b43a64/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                         imageAlt: "Immagine di esempio",
                         label: "Discover Now",
                         link: "/it/being-a-woman-today"
@@ -46,7 +46,7 @@ export default defineEventHandler(() => {
                     {
                         title: "Family ties",
                         text: "For Travis and Cynhyia, respect for nature begins with family and a desire to instill in their son Tenoch a deep understanding of the resposibility that all generations share in caring for the naturl world.",
-                        image: "https://picsum.photos/400/300",
+                        image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/51b4a9b0-43d6-4896-ad39-8b8fa30d8c30/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                         imageAlt: "Immagine di esempio",
                         label: "Discover Now",
                         link: "/it/family-ties"
@@ -54,7 +54,7 @@ export default defineEventHandler(() => {
                     {
                         title: "An Enchanted Archive Tale",
                         text: "At Woolrich, we believe in spreading love, embracing diversity, and standing togheter. In celebration of Pride Month, we have collaborated with the talented Jerome Lamaar to add a touch of enchantment to our iconic archive pieces.",
-                        image: "https://picsum.photos/400/300",
+                        image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/4d6b7428-f59e-461e-806a-d182a30aad8c/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                         imageAlt: "Immagine di esempio",
                         label: "Discover Now",
                         link: "/it/an-enchanted-archive-tale"
@@ -62,7 +62,7 @@ export default defineEventHandler(() => {
                     {
                         title: "Without Barriers",
                         text: "Stories studded with positivity where overcoming personal and social obstacles reflects the truest spirit of human perseverance. This is Without Barriers.",
-                        image: "https://picsum.photos/400/300",
+                        image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/e9bb922c-0741-47be-856f-6b5844e8ea97/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                         imageAlt: "Immagine di esempio",
                         label: "Discover Now",
                         link: "/it/without-barriers"
@@ -70,7 +70,7 @@ export default defineEventHandler(() => {
                     {
                         title: "Growing Up in Italy",
                         text: "A project conceived in collaboration with NSS magazine and The Good Neighborhood Collective to showcase positive stories of integration and success of second-generation italians.",
-                        image: "https://picsum.photos/400/300",
+                        image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/45a51318-a02c-4091-abfe-6e8bbd67aef8/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                         imageAlt: "Immagine di esempio",
                         label: "Discover Now",
                         link: "/it/growing-up-in-italy"
@@ -78,7 +78,7 @@ export default defineEventHandler(() => {
                     {
                         title: "Woolrich Keeps New York Warm",
                         text: "Small businesses have faced challenges during the pandemic: starting out in New York Citu, Woolrich empowers the people and places that are the backbone of the city by shining a light on six small businesses.",
-                        image: "https://picsum.photos/400/300",
+                        image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/31b7bc53-8002-4137-bc5e-6e5fc4140d61/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                         imageAlt: "Immagine di esempio",
                         label: "Discover Now",
                         link: "/it/woolrich-keeps-new-york-warm"
@@ -88,7 +88,7 @@ export default defineEventHandler(() => {
             {
                 name: "ListingPage",
                 title: "Bran Mission and Values",
-                image: "https://picsum.photos/1200/600",
+                image: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/2c40e3e6-9421-4ee1-bad2-a26df967b4d6/fdynkn/std/1024x960/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
                 imageAlt: "Immagine di esempio",
                 btnLabel: "Discover More",
                 btnLink: "/it/stories",
