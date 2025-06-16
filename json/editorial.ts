75d22823ec2b111af4979f3d1b4302e012f76f05
Andrea Margutti <marguttiandrea30@gmail.com>
2025-06-16 15:26:33 +0200

    feat: add CardGallery section with multiple stories to editorial data structure

--- json/editorial.ts
+++ json/editorial.ts
@@ -37,66 +37,66 @@ const editorial: Record<string, unknown> = {
       fontStyle: "capitalized",
       positioningY: "bottom",
     },
-    // {
-    //   name: "CardGallery",
-    //   title: "Inclusive and purposeful stories",
-    //   cards: [
-    //     {
-    //       title: "Being a woman today",
-    //       text: "For this international Women's Day, we recognize five women who are making strides in their own right. Carolina Amoretti, Luzai, Sara Ricciardi, Aya Mohamed, and Bianca Hirata let us into their private spaces and discuss various aspects of womanhood that inspire them",
-    //       image:
-    //         "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/ec4c47d3-3ce6-40c5-aa23-d9e1c9b43a64/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
-    //       imageAlt: "Immagine di esempio",
-    //       label: "Discover Now",
-    //       link: "/it/being-a-woman-today",
-    //     },
-    //     {
-    //       title: "Family ties",
-    //       text: "For Travis and Cynhyia, respect for nature begins with family and a desire to instill in their son Tenoch a deep understanding of the resposibility that all generations share in caring for the naturl world.",
-    //       image:
-    //         "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/51b4a9b0-43d6-4896-ad39-8b8fa30d8c30/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
-    //       imageAlt: "Immagine di esempio",
-    //       label: "Discover Now",
-    //       link: "/it/family-ties",
-    //     },
-    //     {
-    //       title: "An Enchanted Archive Tale",
-    //       text: "At Woolrich, we believe in spreading love, embracing diversity, and standing togheter. In celebration of Pride Month, we have collaborated with the talented Jerome Lamaar to add a touch of enchantment to our iconic archive pieces.",
-    //       image:
-    //         "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/4d6b7428-f59e-461e-806a-d182a30aad8c/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
-    //       imageAlt: "Immagine di esempio",
-    //       label: "Discover Now",
-    //       link: "/it/an-enchanted-archive-tale",
-    //     },
-    //     {
-    //       title: "Without Barriers",
-    //       text: "Stories studded with positivity where overcoming personal and social obstacles reflects the truest spirit of human perseverance. This is Without Barriers.",
-    //       image:
-    //         "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/e9bb922c-0741-47be-856f-6b5844e8ea97/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
-    //       imageAlt: "Immagine di esempio",
-    //       label: "Discover Now",
-    //       link: "/it/without-barriers",
-    //     },
-    //     {
-    //       title: "Growing Up in Italy",
-    //       text: "A project conceived in collaboration with NSS magazine and The Good Neighborhood Collective to showcase positive stories of integration and success of second-generation italians.",
-    //       image:
-    //         "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/45a51318-a02c-4091-abfe-6e8bbd67aef8/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
-    //       imageAlt: "Immagine di esempio",
-    //       label: "Discover Now",
-    //       link: "/it/growing-up-in-italy",
-    //     },
-    //     {
-    //       title: "Woolrich Keeps New York Warm",
-    //       text: "Small businesses have faced challenges during the pandemic: starting out in New York Citu, Woolrich empowers the people and places that are the backbone of the city by shining a light on six small businesses.",
-    //       image:
-    //         "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/31b7bc53-8002-4137-bc5e-6e5fc4140d61/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
-    //       imageAlt: "Immagine di esempio",
-    //       label: "Discover Now",
-    //       link: "/it/woolrich-keeps-new-york-warm",
-    //     },
-    //   ],
-    // },
+    {
+      name: "CardGallery",
+      title: "Inclusive and purposeful stories",
+      cards: [
+        {
+          title: "Being a woman today",
+          text: "For this international Women's Day, we recognize five women who are making strides in their own right. Carolina Amoretti, Luzai, Sara Ricciardi, Aya Mohamed, and Bianca Hirata let us into their private spaces and discuss various aspects of womanhood that inspire them",
+          image:
+            "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/ec4c47d3-3ce6-40c5-aa23-d9e1c9b43a64/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
+          imageAlt: "Immagine di esempio",
+          label: "Discover Now",
+          link: "/it/being-a-woman-today",
+        },
+        {
+          title: "Family ties",
+          text: "For Travis and Cynhyia, respect for nature begins with family and a desire to instill in their son Tenoch a deep understanding of the resposibility that all generations share in caring for the naturl world.",
+          image:
+            "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/51b4a9b0-43d6-4896-ad39-8b8fa30d8c30/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
+          imageAlt: "Immagine di esempio",
+          label: "Discover Now",
+          link: "/it/family-ties",
+        },
+        {
+          title: "An Enchanted Archive Tale",
+          text: "At Woolrich, we believe in spreading love, embracing diversity, and standing togheter. In celebration of Pride Month, we have collaborated with the talented Jerome Lamaar to add a touch of enchantment to our iconic archive pieces.",
+          image:
+            "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/4d6b7428-f59e-461e-806a-d182a30aad8c/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
+          imageAlt: "Immagine di esempio",
+          label: "Discover Now",
+          link: "/it/an-enchanted-archive-tale",
+        },
+        {
+          title: "Without Barriers",
+          text: "Stories studded with positivity where overcoming personal and social obstacles reflects the truest spirit of human perseverance. This is Without Barriers.",
+          image:
+            "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/e9bb922c-0741-47be-856f-6b5844e8ea97/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
+          imageAlt: "Immagine di esempio",
+          label: "Discover Now",
+          link: "/it/without-barriers",
+        },
+        {
+          title: "Growing Up in Italy",
+          text: "A project conceived in collaboration with NSS magazine and The Good Neighborhood Collective to showcase positive stories of integration and success of second-generation italians.",
+          image:
+            "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/45a51318-a02c-4091-abfe-6e8bbd67aef8/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
+          imageAlt: "Immagine di esempio",
+          label: "Discover Now",
+          link: "/it/growing-up-in-italy",
+        },
+        {
+          title: "Woolrich Keeps New York Warm",
+          text: "Small businesses have faced challenges during the pandemic: starting out in New York Citu, Woolrich empowers the people and places that are the backbone of the city by shining a light on six small businesses.",
+          image:
+            "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/31b7bc53-8002-4137-bc5e-6e5fc4140d61/fdynkn/std/381x1280/editorialimage?format=auto&scalemode=none&quality=80&dpr=120",
+          imageAlt: "Immagine di esempio",
+          label: "Discover Now",
+          link: "/it/woolrich-keeps-new-york-warm",
+        },
+      ],
+    },
     {
       name: "HeroBanner",
       variant: "buttons",
