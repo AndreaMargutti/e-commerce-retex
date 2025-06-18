2f287d3340bb5a374f7b2885c103b91d7f0818c7
Andrea Margutti <marguttiandrea30@gmail.com>
2025-06-18 12:17:59 +0200

    feat: add SplitSection component and MoleculesCard stories for Storybook

---
+++ layers/design-system/components/Molecules/Card/MoleculesCard.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+// If the file is named 'MoleculesCard.vue'
+import MoleculesCard from "./MoleculesCard.vue";
+
+const meta: Meta<typeof MoleculesCard> = {
+  title: "Design System /Molecules / Card",
+  component: MoleculesCard,
+  argTypes: {
+    title: { control: "text" },
+    subtitle: { control: "text" },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {
+  args: {
+    title: "Card Title",
+    subtitle: "Card Subtitle",
+    image: {
+      src: "https://woolrich-cdn.thron.com/delivery/public/image/woolrich/83c3b993-ca3a-4392-8d22-6f02e59d2cb0/fdynkn/std/928x960/editorialimage?format=auto&scalemode=none&quality=100&dpr=120",
+      alt: "Placeholder Image",
+    },
+    labels: [
+      { label: "Label 1", href: "#" },
+      { label: "Label 2", href: "#" },
+    ],
+  },
+  render: (args) => ({
+    components: { MoleculesCard },
+    setup() {
+      return { args };
+    },
+    template: `<MoleculesCard v-bind="args" />`,
+  }),
+};
