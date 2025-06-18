6217ecfd436d3b77507a6853091d5eaa29c34d05
Andrea Margutti <marguttiandrea30@gmail.com>
2025-06-18 11:23:40 +0200

    feat: add gap to CardGallery layout and enhance MoleculesCard styling with text sizes

--- layers/design-system/components/Molecules/Card/MoleculesCard.vue
+++ layers/design-system/components/Molecules/Card/MoleculesCard.vue
@@ -8,8 +8,12 @@ defineProps<MoleculesCardProps>();
     <figure class="pb-[1.4rem]">
       <img :src="image.src" :alt="image.alt" class="aspect-square w-full" />
       <div class="px-6 pt-6 pb-5">
-        <AtomsTitle :title="title" color="black" class="pb-2" />
-        <AtomsSubtitle :subtitle="subtitle ?? ''" color="black" class="pb-0" />
+        <AtomsTitle :title="title" color="black" class="pb-2 text-md" />
+        <AtomsSubtitle
+          :subtitle="subtitle ?? ''"
+          color="black"
+          class="pb-0 text-sm"
+        />
       </div>
       <div class="flex gap-8 px-6 *:p-0">
         <AtomsLink
