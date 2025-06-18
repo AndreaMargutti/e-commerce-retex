b53c0a715e722f91551fdb2d6c6c4b1424eae5dc
Andrea Margutti <marguttiandrea30@gmail.com>
2025-06-18 12:07:32 +0200

    feat: conditionally render title in TextBlock component

--- layers/design-system/components/Modules/TextBlock/TextBlock.vue
+++ layers/design-system/components/Modules/TextBlock/TextBlock.vue
@@ -8,7 +8,7 @@ defineProps<TextBlockProps>();
   <div
     class="bg-white-state text-black-base font-medium text-sm md:text-md leading-[17px] text-center px-10 md:px-56 py-10"
   >
-    <h2 class="font-normal pb-[24px]">{{ title }}</h2>
+    <h2 v-if="title" class="font-normal pb-[24px]">{{ title }}</h2>
     <span>{{ text }}</span>
   </div>
 </template>
