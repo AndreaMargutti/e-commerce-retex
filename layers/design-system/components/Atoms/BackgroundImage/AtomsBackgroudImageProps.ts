63483e9164d571bc29ca0eef70aa8a9d5d57c1d1
Andrea Margutti <marguttiandrea30@gmail.com>
2025-07-09 12:38:53 +0200

    refactor: remove AtomsBackgroundImage component and replace with NuxtImg in HeroBanner

--- layers/design-system/components/Atoms/BackgroundImage/AtomsBackgroudImageProps.ts
+++
@@ -1,4 +0,0 @@
-export type AtomsBackgroundImageProps = {
-  src: string;
-  alt: string;
-};
--- layers/design-system/components/Modules/HeroBanner/HeroBanner.vue
+++ layers/design-system/components/Modules/HeroBanner/HeroBanner.vue
@@ -60,7 +60,11 @@ const labelPosition = computed(() => {
 
 <template>
   <figure class="relative">
-    <AtomsBackgroundImage :src="backgroundImage" :alt="backgroundImageAlt" />
+    <NuxtImg
+      :src="backgroundImage"
+      :alt="backgroundImageAlt"
+      class="object-cover object-[25%] aspect-[3/4] md:object-center md:aspect-[12/5] w-full"
+    />
     <AtomsGradient />
     <section :class="positioning" class="absolute py-15 px-8 max-w-1/2">
       <div>
