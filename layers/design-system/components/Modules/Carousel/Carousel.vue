ba70e19ba293923ff3a0acb695bb0f2d0a6d9a21
Andrea Margutti <marguttiandrea30@gmail.com>
2025-07-14 12:27:24 +0200

    feat: implement Carousel component with navigation and slide rendering

--- layers/design-system/components/Modules/Carousel/Carousel.vue
+++ layers/design-system/components/Modules/Carousel/Carousel.vue
@@ -1,7 +1,42 @@
 <script lang="ts" setup>
 import type { CarouselProps } from "./CarouselProps";
+import emblaCarouselVue from "embla-carousel-vue";
 
+const [emblaRef, emblaApi] = emblaCarouselVue();
+function scrollNext() {
+  emblaApi.value?.scrollNext();
+}
+function scrollPrev() {
+  emblaApi.value?.scrollPrev();
+}
 defineProps<CarouselProps>();
 </script>
 
-<template>Ciao sono il carosello</template>
+<template>
+  <div class="embla">
+    <div ref="emblaRef" class="embla__viewport">
+      <div class="embla__container">
+        <div v-for="slide in slides" :key="slide.id" class="embla__slide">
+          <NuxtImg :src="slide.url" :alt="slide.alt" class="w-full" />
+        </div>
+      </div>
+    </div>
+    <button class="embla__prev" @click="scrollPrev">Prev</button>
+    <button class="embla__next" @click="scrollNext">Next</button>
+  </div>
+</template>
+
+<style scoped>
+.embla {
+  overflow: hidden;
+}
+
+.embla__container {
+  display: flex;
+}
+
+.embla__slide {
+  flex: 0 0 100%; /* Slide covers 80% of the viewport */
+  min-width: 0;
+}
+</style>
