58146c20f06f51daeb19d62baecaff60ac33cf68
Andrea Margutti <marguttiandrea30@gmail.com>
2025-07-14 10:03:24 +0200

    fix: adjust CardGallery grid columns and update TextBlock max-width styling

--- layers/design-system/components/Modules/CardGallery/CardGallery.vue
+++ layers/design-system/components/Modules/CardGallery/CardGallery.vue
@@ -8,7 +8,7 @@ defineProps<CardGalleryProps>();
   <h2 class="black text-center pt-11 pb-7 uppercase text-2-5xl leading-[37px]">
     {{ title }}
   </h2>
-  <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-2">
+  <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
     <MoleculesCard
       v-for="card in cards"
       :key="card.title"
--- layers/design-system/components/Modules/TextBlock/TextBlock.vue
+++ layers/design-system/components/Modules/TextBlock/TextBlock.vue
@@ -6,7 +6,7 @@ defineProps<TextBlockProps>();
 
 <template>
   <div
-    class="bg-white-state text-black-base font-medium text-md leading-[17px] text-center px-10 py-10"
+    class="bg-white-state text-black-base font-medium text-md leading-[17px] text-center px-10 py-10 max-w-[80%] mx-auto"
   >
     <h2 v-if="title" class="font-normal pb-[24px]">{{ title }}</h2>
     <p class="leading-[21px]">{{ text }}</p>
