7d2de989b95d621b3fb69a75e299b31b962a786e
Andrea Margutti <marguttiandrea30@gmail.com>
2025-07-16 17:24:54 +0200

    fix: correct grid layout and improve card alignment in CenterCard component

--- layers/design-system/components/Modules/CenterCard/CenterCard.vue
+++ layers/design-system/components/Modules/CenterCard/CenterCard.vue
@@ -5,13 +5,17 @@ defineProps<CenterCardProps>();
 </script>
 
 <template>
-  <div class="grid cols-cols-1 grid-cols-4 gap-4">
+  <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
     <div v-for="card in cards" :key="card.title" class="center-card">
       <NuxtImg :src="card.image.src" :alt="card.image.alt" class="w-full" />
-      <div class="flex flex-col gap-4">
-        <h4 v-if="card.appendix">{{ card.appendix }}</h4>
-        <h2>{{ card.title }}</h2>
-        <div class="flex">
+      <div class="flex flex-col items-center">
+        <h4 v-if="card.appendix" class="pt-6 uppercase text-sm leading-[17px]">
+          {{ card.appendix }}
+        </h4>
+        <h2 class="py-6 text-2-5xl leading-[34px]">
+          {{ capitalize(card.title) }}
+        </h2>
+        <div class="flex pb-6">
           <AtomsLink
             :name="card.link.label"
             :href="card.link.href"
