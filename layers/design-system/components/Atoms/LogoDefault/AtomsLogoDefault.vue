0a9f78776fd1fe359789c7a8245f71cdc055bfe1
AndreaMargutti <marguttiandrea30@gmail.com>
2025-08-06 11:12:27 +0200

    fix: remove SVG logo and replace with text for improved accessibility

--- layers/design-system/components/Atoms/LogoDefault/AtomsLogoDefault.vue
+++ layers/design-system/components/Atoms/LogoDefault/AtomsLogoDefault.vue
@@ -1,8 +1,5 @@
 <template>
   <NuxtLink to="http://localhost:3000/">
-    <Icon
-      name="icon:logo-default"
-      class="text-[4rem] lg:text-[51px] xl:text-[4.5rem]"
-    />
+    <h1 class="font-bold">WOREALIS</h1>
   </NuxtLink>
 </template>
