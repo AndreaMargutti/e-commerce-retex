36fa0f3a76a463f063abcacd375eef45137c440f
AndreaMargutti <marguttiandrea30@gmail.com>
2025-08-06 11:58:52 +0200

    fix: enhance logo text styling for improved visibility

--- layers/design-system/components/Atoms/LogoDefault/AtomsLogoDefault.vue
+++ layers/design-system/components/Atoms/LogoDefault/AtomsLogoDefault.vue
@@ -1,5 +1,5 @@
 <template>
   <NuxtLink to="http://localhost:3000/">
-    <h1 class="font-bold">WOREALIS</h1>
+    <h1 class="font-bold text-2-5xl leading-[1.15]">WOREALIS</h1>
   </NuxtLink>
 </template>
